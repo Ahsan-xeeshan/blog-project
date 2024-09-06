d6f48d6e74ecafd1f5defaee3c2e2c402c9b3324
Nazmul Ahsan <xeeshann.ahsan@gmail.com>
2024-09-07 03:48:49 +0600

    bcrypt added

--- Server/controllers/registrationController.js
+++ Server/controllers/registrationController.js
@@ -1,19 +1,48 @@
 const userSchema = require("../models/userSchema");
+const bcrypt = require("bcrypt");
+
 async function registrationController(req, res) {
-  try {
-    const { username, email, password } = req.body;
-    const userData = new userSchema({
+  let emailInput = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+  let passwordInput =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+  const { username, email, password } = req.body;
+
+  if (!username) {
+    return res.json({ error: "Name is required" });
+  }
+  if (!email) {
+    return res.json({ error: "Email is required" });
+  } else if (!emailInput.test(email)) {
+    return res.json({ error: "Name is Invalid" });
+  }
+
+  if (!password) {
+    return res.json({ error: "Password is required" });
+  } else {
+    if (!passwordInput.test(password)) {
+      return res.json({
+        error:
+          "#Password should be between 8 to 15 characters which contain at least one lowercase letter[a-z], one uppercase letter[A-Z], one numeric digit[0-9], and one special character[!,@,#,$,%,^,&,*]",
+      });
+    }
+  }
+  const existingEmail = await userSchema.find({ email });
+
+  if (existingEmail.length > 0) {
+    res.json({ error: "This email is already used" });
+  }
+
+  bcrypt.hash(password, 10, function (err, hash) {
+    const users = new userSchema({
       username,
       email,
-      password,
+      password: hash,
     });
-    await userData.save();
-
-    res.status(201).json({ success: "Registration successfully" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error" });
-  }
+    users.save();
+    res.send({
+      success: "Registration Successfully done.",
+    });
+  });
 }
 
 module.exports = {
