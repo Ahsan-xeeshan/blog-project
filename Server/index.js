29ce67962fa3bce37db12b4492c848d24bd7ba26
Nazmul Ahsan <xeeshann.ahsan@gmail.com>
2024-09-07 02:26:13 +0600

    connect with mongodb

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+const dbConnection = require("./config/dbConnection");
 const app = express();
 const port = 3000;
 
@@ -6,6 +8,12 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+dbConnection();
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
