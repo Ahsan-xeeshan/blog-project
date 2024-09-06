58d534fe67ecc495865e2aec5db931b848a2196d
Nazmul Ahsan <xeeshann.ahsan@gmail.com>
2024-09-06 22:44:54 +0600

    react router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "../src/Pages/About";
+import Dashboard from "../src/Pages/Dashboard";
+import Home from "../src/Pages/Home";
+import Projects from "../src/Pages/Projects";
+import Signin from "../src/Pages/Signin";
+import Signup from "../src/Pages/Signup";
 function App() {
   return (
     <>
-      <h1 className="bg-red-500">Hello Bloggers</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
