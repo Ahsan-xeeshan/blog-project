019279eb59e5cc18580f2b4880ceb692ef7a8ef0
Xeeshan <xeeshann.ahsan@gmail.com>
2024-09-25 00:51:12 +0600

    static path

--- Server/index.js
+++ Server/index.js
@@ -5,16 +5,22 @@ const dbConnection = require("./config/dbConnection");
 const cookieParser = require("cookie-parser");
 const app = express();
 const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const path = require("path");
 
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+if (process.env.NODE_ENV === "production") {
+  //*Set static folder up in production
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.use(route);
 
