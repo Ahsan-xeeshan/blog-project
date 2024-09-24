7778037299a09cfb2e8e67a49bd836075dc4f546
Xeeshan <xeeshann.ahsan@gmail.com>
2024-09-25 01:08:26 +0600

    dist file

--- Server/index.js
+++ Server/index.js
@@ -13,16 +13,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-if (process.env.NODE_ENV === "production") {
-  //*Set static folder up in production
-  app.use(express.static("client"));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "index.html"))
-  );
-}
-
 app.use(route);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 dbConnection();
 app.listen(port, () => {
